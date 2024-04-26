20ddc0aa6bd8be1d11ac178461727614902730b2
Paul Palmer <p.j.palmer@tech-trends.co.uk>
2024-04-26 10:32:55 +0100

    Update get.tag.value.pairs.R

--- R/get.tag.value.pairs.R
+++ R/get.tag.value.pairs.R
@@ -9,18 +9,31 @@
 #'
 #'
 get.tag.value.pairs <- function(x) {
-  tag.value.pairs <- x %>%
-    triple.as.wide() %>%
-    dplyr::filter(.data$tag.type == "TaggedValue") %>%
-    dplyr::select(.data$datumEntity, .data$tag, .data$value) %>%
-    tidyr::pivot_wider(
-      id_cols = .data$datumEntity,
-      names_from = .data$tag,
-      values_from = .data$value
-    ) %>%
-    data.as.triple()
 
-  x.out <- rbind(x, tag.value.pairs)
+  wide.x <- x %>% triple.as.wide()
+
+  tag.present <- "TaggedValue" %in% colnames( wide.x)
+
+  if( tag.present) {
+    tag.value.pairs <- x %>%
+      triple.as.wide() %>%
+      dplyr::filter(.data$tag.type == "TaggedValue") %>%
+      dplyr::select(.data$datumEntity, .data$tag, .data$value) %>%
+      tidyr::pivot_wider(
+        id_cols = .data$datumEntity,
+        names_from = .data$tag,
+        values_from = .data$value
+      ) %>%
+      data.as.triple()
+
+    x.out <- rbind(x, tag.value.pairs)
+
+  }else{
+    x.out <- x
+
+  }
+
+
 
   return(x.out)
 }
